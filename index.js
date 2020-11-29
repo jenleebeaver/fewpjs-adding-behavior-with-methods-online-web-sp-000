53068c9e0fcc383a666ea5bbba7f1ad7286bf1f6
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-29 04:46:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ class Cat {
     this.name = name;
     this.sex = sex
   }
-}
+},
 
 class Dog {
-  //...
-}
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex
+  }
+},
 
 class Bird {
-  //...
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex
+  }
 }
